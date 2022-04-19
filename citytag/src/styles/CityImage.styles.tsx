7f296ccb811a8b02077c120188b375047beb42e3
abstractdev <fadcny@gmail.com>
2022-04-18 23:03:12 -0400

    moved CityImage components

---
+++ citytag/src/styles/CityImage.styles.tsx
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+import { VFlexContainer } from "./VFlexContainer.styles"
+
+export const CityImage = styled.img`
+  max-width: 100%;
+  height: auto;
+  object-fit: contain;
+` 
+export const CityImageContainer = styled(VFlexContainer)`
+  width: 100%;
+  height: auto;
+`
