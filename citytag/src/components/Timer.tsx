a97f1e2f17235164ea63f3b22b7de2839c90ce6f
abstractdev <fadcny@gmail.com>
2022-04-21 01:49:30 -0400

    basic timer display complete

--- citytag/src/components/Timer.tsx
+++ citytag/src/components/Timer.tsx
@@ -10,12 +10,12 @@ export function Timer(props: TimerProps) {
   return (
     <>
       <TimerDiv isActive={props.isActive}>
-        <MinutesSpan>{("0" + Math.floor((props.time / 60000) % 60)).slice(-1)}<span style={{fontSize: '70%'}}>m</span>
-        </MinutesSpan>
-        <SecondsSpan>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}<span style={{fontSize: '70%'}}>s</span> 
-        </SecondsSpan>
-        <MillisecondsSpan>{("0" + (props.time % 1000)).slice(-2)}<span style={{fontSize: '70%'}}>ms</span>
-        </MillisecondsSpan>
+        <TimerSpan>{("0" + Math.floor((props.time / 60000) % 60)).slice(-1)}<span style={{fontSize: '70%'}}>min</span>
+        </TimerSpan>
+        <TimerSpan>{("0" + Math.floor((props.time / 1000) % 60)).slice(-2)}<span style={{fontSize: '70%'}}>.</span> 
+        </TimerSpan>
+        <TimerSpan>{("0" + (props.time % 1000)).slice(-2)}<span style={{fontSize: '70%'}}>s</span>
+        </TimerSpan>
       </TimerDiv>
     </>
   );
@@ -26,22 +26,15 @@ interface TimerDivProps {
 }
 
 const TimerDiv = styled.div<TimerDivProps>`
-  display: flex;
-  justify-content: center;
-  align-content: center;
+  font-family: 'metropolis';
   padding: .2em;
-  border: ${(props) => (!props.isActive ? "3px solid #FFF" : "none")};
+  border: ${(props) => (!props.isActive ? "1px solid #FFF" : "none")};
   border-radius: ${(props) => (!props.isActive ? "5px" : "none")};
   @media screen and (max-width: 630px){
     font-size: .6rem;
   }
 `
-const MillisecondsSpan = styled.span`
+const TimerSpan = styled.span`
 color: #FFF;
-`
-const SecondsSpan = styled.span`
-color: #FFF;
-`
-const MinutesSpan = styled.span`
-color: #FFF;
+font-weight: 900;
 `
