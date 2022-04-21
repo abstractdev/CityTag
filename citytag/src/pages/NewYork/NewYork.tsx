b10fb07549eee7bd1f56ecc3a84a3f1c0ba85fc2
abstractdev <fadcny@gmail.com>
2022-04-21 01:57:17 -0400

    new york visual elements complete.

---
+++ citytag/src/pages/NewYork/NewYork.tsx
@@ -0,0 +1,175 @@
+import ny from "../../assets/images/ny/ny.jpg";
+import styled from "styled-components";
+import { VFlexContainer } from "../../styles/VFlexContainer.styles";
+import { CityImage } from "../../styles/CityImage.styles";
+import { NewYorkFind } from "../../components/Find";
+import { CityImageContainer } from "../../styles/CityImage.styles";
+import { NewYorkDropdown } from "./NewYorkDropdown";
+import { useEffect } from "react";
+// import { setDoc, doc } from "firebase/firestore";
+// import { db } from "../../components/Firebase";
+import { CityProps } from "../../Interfaces";
+
+export function NewYork(props: CityProps) {
+  const {
+    mouseX,
+    mouseY,
+    imageIsClicked,
+    clickHistory,
+    broadwayIsFound,
+    hotdogIsFound,
+    ilovenyIsFound,
+    policeIsFound,
+    isActive,
+    time,
+    setTime,
+    setIsActive,
+    setBroadwayIsFound,
+    setHotdogIsFound,
+    setIlovenyIsFound,
+    setPoliceIsFound,
+    setClickHistory,
+    handleMouseClickPosition,
+    checkFirebaseForMatch,
+  } = props;
+  const broadwayText = "Broadway Sign";
+  const hotdogText = "Hot Dog Vendor";
+  const ilovenyText = "I Love NY Shirt";
+  const policeText = "NYPD Officer";
+
+  // useEffect(() => {
+  //   let interval: NodeJS.Timer;
+  //   if (isActive) {
+
+  //     const docRef = doc(db, "nyUsers", id);
+  //     setDoc(docRef,{id: id})
+  //     console.log("Document written");
+
+  //     interval = setInterval(() => {
+  //       setTime((prev) => prev + 10);
+  //     }, 10);
+  //   } else if (!isActive) {
+  //     clearInterval(interval);
+  //     const userRef = doc(db, 'nyUsers', id);
+  //     setDoc(userRef, { time: time }, { merge: true });
+  //   }
+  //   return () => clearInterval(interval);
+  // }, [isActive]);
+
+  useEffect(() => {
+    if (broadwayIsFound && hotdogIsFound && ilovenyIsFound && policeIsFound) {
+      setIsActive(false);
+    }
+  }, [
+    broadwayIsFound,
+    hotdogIsFound,
+    ilovenyIsFound,
+    policeIsFound,
+    setIsActive,
+  ]);
+
+  return (
+    <>
+      <NewYorkContainer>
+        <NewYorkFind
+          broadwayText={broadwayText}
+          hotdogText={hotdogText}
+          ilovenyText={ilovenyText}
+          policeText={policeText}
+          broadwayIsFound={broadwayIsFound}
+          hotdogIsFound={hotdogIsFound}
+          ilovenyIsFound={ilovenyIsFound}
+          policeIsFound={policeIsFound}
+        />
+        <CityImageContainer
+          onClick={(event) => handleMouseClickPosition(event)}
+        >
+          <CityImage src={ny} />
+          <BroadwayDiv
+            broadwayIsFound={broadwayIsFound}
+            data-id="broadwayDiv"
+            onClick={(event) => checkFirebaseForMatch(event, "nyFindImages")}
+          ></BroadwayDiv>
+          <HotdogDiv
+            hotdogIsFound={hotdogIsFound}
+            data-id="hotdogDiv"
+            onClick={(event) => checkFirebaseForMatch(event, "nyFindImages")}
+          ></HotdogDiv>
+          <IlovenyDiv
+            ilovenyIsFound={ilovenyIsFound}
+            data-id="ilovenyDiv"
+            onClick={(event) => checkFirebaseForMatch(event, "nyFindImages")}
+          ></IlovenyDiv>
+          <PoliceDiv
+            policeIsFound={policeIsFound}
+            data-id="policeDiv"
+            onClick={(event) => checkFirebaseForMatch(event, "nyFindImages")}
+          ></PoliceDiv>
+          <NewYorkDropdown
+            mouseX={mouseX}
+            mouseY={mouseY}
+            broadwayText={broadwayText}
+            hotdogText={hotdogText}
+            ilovenyText={ilovenyText}
+            policeText={policeText}
+            imageIsClicked={imageIsClicked}
+            clickHistory={clickHistory}
+            setClickHistory={setClickHistory}
+            setBroadwayIsFound={setBroadwayIsFound}
+            setHotdogIsFound={setHotdogIsFound}
+            setIlovenyIsFound={setIlovenyIsFound}
+            setPoliceIsFound={setPoliceIsFound}
+          />
+        </CityImageContainer>
+      </NewYorkContainer>
+    </>
+  );
+}
+
+//STYLED COMPONENTS//
+interface DivProps {
+  broadwayIsFound?: boolean;
+  hotdogIsFound?: boolean;
+  ilovenyIsFound?: boolean;
+  policeIsFound?: boolean;
+}
+
+const NewYorkContainer = styled(VFlexContainer)`
+  background-color: #f2c205;
+`;
+const BroadwayDiv = styled.div<DivProps>`
+  width: 7%;
+  height: 6.2%;
+  position: absolute;
+  left: 53.5%;
+  bottom: 84.2%;
+  border: ${(props) => (props.broadwayIsFound ? "5px solid black" : "none")};
+  border-radius: 5px;
+`;
+const HotdogDiv = styled.div<DivProps>`
+  width: 3%;
+  height: 9%;
+  position: absolute;
+  left: 15.5%;
+  bottom: 32%;
+  border: ${(props) => (props.hotdogIsFound ? "5px solid black" : "none")};
+  border-radius: 5px;
+`;
+const IlovenyDiv = styled.div<DivProps>`
+  width: 2.7%;
+  height: 9%;
+  position: absolute;
+  left: 46%;
+  bottom: 42%;
+  border: ${(props) => (props.ilovenyIsFound ? "5px solid black" : "none")};
+  border-radius: 5px;
+`;
+const PoliceDiv = styled.div<DivProps>`
+  width: 3%;
+  height: 8%;
+  position: absolute;
+  left: 52%;
+  bottom: 61%;
+  border: ${(props) => (props.policeIsFound ? "5px solid black" : "none")};
+  border-radius: 5px;
+`;
---
+++ citytag/src/pages/NewYork/NewYorkDropdown.tsx
@@ -0,0 +1,119 @@
+import styled from "styled-components";
+import { DropdownProps } from "../../Interfaces";
+import { TargetBox } from "../../styles/Dropdown.styles";
+import { DropdownContainer } from "../../styles/Dropdown.styles";
+import { DropdownLi } from "../../styles/Dropdown.styles";
+
+export function NewYorkDropdown(props: DropdownProps) {
+  const {
+    mouseX,
+    mouseY,
+    imageIsClicked,
+    clickHistory,
+    setClickHistory,
+    broadwayText,
+    hotdogText,
+    ilovenyText,
+    policeText,
+    setBroadwayIsFound,
+    setHotdogIsFound,
+    setIlovenyIsFound,
+    setPoliceIsFound,
+  } = props;
+  const adjBoxY = mouseY - 22;
+  const adjBoxX = mouseX - 22;
+  const adjDropdownY = mouseY + 17;
+  const adjDropdownX = mouseX + 17;
+
+  function checkIfDivAndDropdownMatch(event: any) {
+    if (clickHistory.includes(`${event.target.dataset.id}Div`)) {
+      switch (event.target.dataset.id) {
+        case "broadway":
+          setBroadwayIsFound(true);
+          break;
+        case "hotdog":
+          setHotdogIsFound(true);
+          break;
+        case "iloveny":
+          setIlovenyIsFound(true);
+          break;
+        case "police":
+          setPoliceIsFound(true);
+          break;
+        default:
+          break;
+      }
+      handleSetClickHistory(event);
+    }
+  }
+
+  function handleSetClickHistory(event: any) {
+    const filtered = clickHistory.filter(
+      (e: string) => e !== `${event.target.dataset.id}Div`
+    );
+    setClickHistory([...filtered, `${event.target.dataset.id}Found`]);
+  }
+
+  return (
+    <>
+      {imageIsClicked && (
+        <>
+          <TargetBoxNewYork
+            style={{
+              top: `${adjBoxY.toString()}px`,
+              left: `${adjBoxX.toString()}px`,
+            }}
+          />
+          <DropdownContainerNewYork
+            style={{
+              top: `${adjDropdownY.toString()}px`,
+              left: `${adjDropdownX.toString()}px`,
+            }}
+          >
+            <ul>
+              <DropdownLiNewYork
+                data-id="broadway"
+                onClick={(event) => checkIfDivAndDropdownMatch(event)}
+              >
+                {broadwayText}
+              </DropdownLiNewYork>
+              <DropdownLiNewYork
+                data-id="hotdog"
+                onClick={(event) => checkIfDivAndDropdownMatch(event)}
+              >
+                {hotdogText}
+              </DropdownLiNewYork>
+              <DropdownLiNewYork
+                data-id="iloveny"
+                onClick={(event) => checkIfDivAndDropdownMatch(event)}
+              >
+                {ilovenyText}
+              </DropdownLiNewYork>
+              <DropdownLiNewYork
+                data-id="police"
+                onClick={(event) => checkIfDivAndDropdownMatch(event)}
+              >
+                {policeText}
+              </DropdownLiNewYork>
+            </ul>
+          </DropdownContainerNewYork>
+        </>
+      )}
+    </>
+  );
+}
+
+//STYLED COMPONENTS//
+const TargetBoxNewYork = styled(TargetBox)`
+  border: 5px solid #f2c205;
+`;
+
+const DropdownContainerNewYork = styled(DropdownContainer)`
+  border: 5px solid #f2c205;
+  background-color: #f2c205;
+  color: #121212;
+`;
+
+const DropdownLiNewYork = styled(DropdownLi)`
+  background-color: #f2c205;
+`;
