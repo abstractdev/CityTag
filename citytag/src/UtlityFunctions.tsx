bde95c6c35a97afdde38f024468e5036623537b2
abstractdev <fadcny@gmail.com>
2022-04-21 23:19:24 -0400

    audio functions and ms conversion function

---
+++ citytag/src/UtlityFunctions.tsx
@@ -0,0 +1,23 @@
+import rightAudio from "../src/assets/sounds/right.wav"
+import wrongAudio from "../src/assets/sounds/wrong.wav"
+import endAudio from "../src/assets/sounds/end.wav"
+
+export function convertMsToDisplayTime(ms: number) {
+  const minutes = ("0" + Math.floor((ms / 60000) % 60)).slice(-1);
+  const seconds = ("0" + Math.floor((ms / 1000) % 60)).slice(-2);
+  const milliseconds = ("0" + (ms % 1000)).slice(-2);
+    
+  return (`${minutes}min ${seconds}.${milliseconds}s`)
+}
+
+export function AudioFunctions() {
+  const right = new Audio(rightAudio);
+  const wrong = new Audio(wrongAudio);
+  const end = new Audio(endAudio);
+
+  return {
+    right,
+    wrong,
+    end
+  }
+}
