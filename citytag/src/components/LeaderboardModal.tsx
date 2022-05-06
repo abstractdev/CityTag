696d8544a90acab3a310247f7a3adff15e2a3210
abstractdev <fadcny@gmail.com>
2022-05-06 19:53:30 -0400

    conditional render modal based on city

--- citytag/src/components/LeaderboardModal.tsx
+++ citytag/src/components/LeaderboardModal.tsx
@@ -1,13 +1,16 @@
 import styled from "styled-components";
 import { ModalContainer, Modal } from "../styles/Modal.styles";
 import { VFlexContainer } from "../styles/VFlexContainer.styles";
-import { LeaderboardTable } from "./LeaderboardTable";
 import { LeaderboardModalCloseButton } from "./LeaderboardModalCloseButton";
 import { LeaderboardModalProps } from "../Interfaces";
 import { useEffect, useRef } from "react";
 import { clickOutsideFunction } from "../UtlityFunctions";
 import { StyledH1 } from "../styles/Leaderboard.styles";
-
+import { NewYorkLeaderboardTable } from "../pages/NewYorkLeaderboard/NewYorkLeaderboardTable";
+import { ParisLeaderboardTable } from "../pages/ParisLeaderboard/ParisLeaderboardTable";
+import { RioLeaderboardTable } from "../pages/RioLeaderboard/RioLeaderboardTable";
+import { TokyoLeaderboardTable } from "../pages/TokyoLeaderboard/TokyoLeaderboardTable";
+import { useNavigate } from "react-router-dom";
 export function LeaderboardModal(props: LeaderboardModalProps) {
   const {
     cityColor,
@@ -15,26 +18,47 @@ export function LeaderboardModal(props: LeaderboardModalProps) {
     cityText,
     leaderboardIsVisible,
     setLeaderboardIsVisible,
+    newyorkUserData,
+    parisUserData,
+    rioUserData,
+    tokyoUserData,
   } = props;
+  const navigate = useNavigate();
   const mainModalRef = useRef();
   useEffect(() => {
     clickOutsideFunction(
       leaderboardIsVisible,
       setLeaderboardIsVisible,
       mainModalRef,
+      navigate
     );
   }, [leaderboardIsVisible]);
   return (
     <>
-      {leaderboardIsVisible && <ModalContainer>
-        <MainModal cityColor={cityColor} ref={mainModalRef}>
-          <LeaderboardModalCloseButton setLeaderboardIsVisible={setLeaderboardIsVisible}/>
-          <VFlexContainer>
-            <StyledH1 cityFont={cityFont}>{cityText}</StyledH1>
-          </VFlexContainer>
-          <LeaderboardTable />
-        </MainModal>
-      </ModalContainer>}
+      {leaderboardIsVisible && (
+        <ModalContainer>
+          <MainModal cityColor={cityColor} ref={mainModalRef}>
+            <LeaderboardModalCloseButton
+              setLeaderboardIsVisible={setLeaderboardIsVisible}
+            />
+            <VFlexContainer>
+              <StyledH1 cityFont={cityFont}>{cityText}</StyledH1>
+            </VFlexContainer>
+            {cityFont === "newyork" && (
+              <NewYorkLeaderboardTable newyorkUserData={newyorkUserData} />
+            )}
+            {cityFont === "paris" && (
+              <ParisLeaderboardTable parisUserData={parisUserData} />
+            )}
+            {cityFont === "rio" && (
+              <RioLeaderboardTable rioUserData={rioUserData} />
+            )}
+            {cityFont === "tokyo" && (
+              <TokyoLeaderboardTable tokyoUserData={tokyoUserData} />
+            )}
+          </MainModal>
+        </ModalContainer>
+      )}
     </>
   );
 }
@@ -45,8 +69,8 @@ const MainModal = styled(Modal)<{ cityColor: string }>`
   justify-content: center;
   align-items: center;
   background-color: ${({ cityColor }) => cityColor};
-  border: 5px solid #FFF;
-  @media screen and (max-width: 644px) {
-    width: 100%
+  border: 5px solid #fff;
+  @media screen and (max-width: 670px) {
+    width: 100%;
   }
 `;
