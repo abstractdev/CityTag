654a27286c12fae7978b0f62ca676f90e9cb580f
abstractdev <fadcny@gmail.com>
2022-05-06 19:39:04 -0400

    added icons, alignment

--- citytag/src/components/Footer.tsx
+++ citytag/src/components/Footer.tsx
@@ -1,10 +1,28 @@
 import styled from "styled-components";
+import { AiOutlineGithub, AiOutlineBehanceSquare } from "react-icons/ai";
 
 export function Footer() {
   return (
     <>
       <StyledFooterContainer>
-        <a href="http://www.behance.net/gusmorais">All images by Gus Morais</a>
+        <StyledDiv>
+          <StyledLink href="https://github.com/abstractdev">
+            <AiOutlineBehanceSquare />
+          </StyledLink>
+          <StyledLink href="http://www.behance.net/gusmorais">
+            {" "}
+            All images by Gus Morais
+          </StyledLink>
+        </StyledDiv>
+        <StyledDiv>
+          <StyledLink href="https://github.com/abstractdev">
+            <AiOutlineGithub />
+          </StyledLink>
+          <StyledLink href="https://github.com/abstractdev">
+            {" "}
+            Site by abstractdev
+          </StyledLink>
+        </StyledDiv>
       </StyledFooterContainer>
     </>
   );
@@ -15,11 +33,24 @@ const StyledFooterContainer = styled.div`
   left: 0;
   bottom: 0;
   display: flex;
+  justify-content: center;
   align-content: center;
   background-color: ${({ theme }) => theme.colors.footer};
-  padding: .5rem .75rem;
+  padding: 0.5rem 0.75rem;
   font-size: 70%;
   @media screen and (max-width: 644px) {
-    font-size: .6rem;
+    font-size: 0.6rem;
   }
+  gap: 2rem;
+`;
+
+const StyledLink = styled.a`
+  text-decoration: none;
+`;
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 0.08rem;
 `;
