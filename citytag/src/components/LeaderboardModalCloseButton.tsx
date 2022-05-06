85a47480d4bff896478ed5e6f145bc5974d229e4
abstractdev <fadcny@gmail.com>
2022-05-06 19:44:07 -0400

    force back/reload on close

--- citytag/src/components/LeaderboardModalCloseButton.tsx
+++ citytag/src/components/LeaderboardModalCloseButton.tsx
@@ -1,14 +1,21 @@
 import { Icon } from "@iconify/react";
 import { CloseContainer } from "../styles/Modal.styles";
 import { LeaderboardModalCloseButtonProps } from "../Interfaces";
+import { useNavigate } from "react-router-dom";
 
-export function LeaderboardModalCloseButton(
-  props: LeaderboardModalCloseButtonProps
-) {
+export function LeaderboardModalCloseButton(props: LeaderboardModalCloseButtonProps) {
   const { setLeaderboardIsVisible } = props;
+  const navigate = useNavigate();
+  function handleSetLeaderboardIsVisible() {
+    setLeaderboardIsVisible(false)
+    navigate(-1);
+    setTimeout(() => {
+      navigate(0);
+    }, 500);
+}
   return (
     <>
-      <CloseContainer onClick={() => setLeaderboardIsVisible(false)}>
+      <CloseContainer onClick={handleSetLeaderboardIsVisible}>
         <Icon icon="fa:close" style={{ display: "block", margin: "auto" }} />
       </CloseContainer>
     </>
--- citytag/src/components/UserModalCloseButton.tsx
+++ citytag/src/components/UserModalCloseButton.tsx
@@ -1,12 +1,21 @@
 import { Icon } from "@iconify/react";
 import { CloseContainer } from "../styles/Modal.styles";
 import { UserModalCloseButtonProps } from "../Interfaces";
+import { useNavigate } from "react-router-dom";
 
 export function UserModalCloseButton(props: UserModalCloseButtonProps) {
-const {setUserModalIsOpen} = props
+  const { setUserModalIsVisible } = props;
+  const navigate = useNavigate();
+  function handleSetUserModalIsVisible() {
+    setUserModalIsVisible(false);
+    navigate(-1);
+    setTimeout(() => {
+      navigate(0);
+    }, 500);
+  }
   return (
     <>
-      <CloseContainer onClick={() => setUserModalIsOpen(false)}>
+      <CloseContainer onClick={handleSetUserModalIsVisible}>
         <Icon icon="fa:close" style={{ display: "block", margin: "auto" }} />
       </CloseContainer>
     </>
